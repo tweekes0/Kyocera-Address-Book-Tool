a5964bd9b0e013b649636a85c0e39fb49573b640
Kitar0s <kitaros@protonmail.com>
2022-03-01 00:55:49 -0500

    test: add test for entry validation function

--- db/entry_test.go
+++ db/entry_test.go
@@ -58,3 +58,25 @@ func TestDisplay(t *testing.T) {
 	}
 }
 
+func TestValidateEntry(t *testing.T) {
+	_, err1 := NewEntry(1, "", "username", "example@example.com")
+	_, err2 := NewEntry(2, "name", "123", "example@example.com")
+	_, err3 := NewEntry(3, "name", "username", "invalid_email.com")
+
+	tt := []struct {
+		description string
+		got         error
+		want        error
+	}{
+		{"test invalid name", err1, ErrInvalidName},
+		{"test invalid username", err2, ErrInvalidUsername},
+		{"test invalid email", err3, ErrInvalidEmail},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.description, func(t *testing.T) {
+			t.Parallel()
+			assertError(t, tc.got, tc.want)
+		})
+	}
+}
